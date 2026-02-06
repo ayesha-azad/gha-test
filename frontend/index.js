21867312780e180302614633f8edb4a902ad0f3d
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-02-06 11:37:44 +0500

    Making changes to frontend

--- frontend/index.js
+++ frontend/index.js
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(
-    `Server running on http://localhost:${port}/ under Ayesha's supervision.`,
+    `Server running on http://localhost:${port}/ under Ayesha Azad's supervision.`,
   );
 });
